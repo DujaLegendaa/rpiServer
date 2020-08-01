c6330b8721128fa1e7330dabe86519ba1d85ad8a
DujaLegendaa <dorizmeo2@gmail.com>
2020-08-01 10:37:35 +0200

    added gpio

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.set('views', path.join(__dirname, 'public'));
 const http = require("http").createServer(app);
 const fs = require("fs")
 var io = require('socket.io')(http)
+var gpio = require("onoff").Gpio
+var outP = new gpio(23, "out");
+
 
 http.listen(8000)
 
@@ -19,10 +22,16 @@ io.sockets.on('connection', (socket) => {// WebSocket Connection
     var lightvalue = 0; //static variable for current status
     socket.on('light', (data) => { //get light switch status from client
       lightvalue = data;
-      if (lightvalue) {
-        console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+      if (lightvalue != outP.readSync()) {
+        outP.writeSync(lightvalue); //turn LED on or off, for now we will just show it in console.log
       }
     });
   }); 
 
 console.log("listening on 8000")
+
+process.on("SIGINT", () => {
+  outP.writeSync(0);
+  outP.unexport();
+  process.exit();
+})
